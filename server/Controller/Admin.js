eaa29639debd3d803190e8c55a95991023704a90
sachin31344 <105095798+sachin31344@users.noreply.github.com>
2024-03-09 16:23:24 +0530

    API CALL SETUP

--- server/Controller/Admin.js
+++ server/Controller/Admin.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const adminModel = require("../Model/Admin");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -16,66 +17,111 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Grab the API key and set the port
+const apiKey = process.env.BLAND_API_KEY;
+const PORT = process.env.PORT || 4000;
+
+
 exports.AdminLogin = async (req, res, next) => {
-  const { email, password } = req.body;
-  try {
-    if (!validator.isEmail(email)) {
-      return res.status(400).json({
-        error: "enter a valid email",
-      });
-    }
-    const admin = await adminModel.findOne({ email: email });
+	const { email, password } = req.body;
+	try {
+		if (!validator.isEmail(email)) {
+			return res.status(400).json({
+				error: "enter a valid email",
+			});
+		}
+		const admin = await adminModel.findOne({ email: email });
 
-    if (!admin) {
-      return res.status(401).json({
-        error: "wrong email or password",
-      });
-    }
+		if (!admin) {
+			return res.status(401).json({
+				error: "wrong email or password",
+			});
+		}
 
-    const match = await bcrypt.compare(password, admin.password);
-    if (!match) {
-      return res.status(400).json({
-        error: "wrong email or password",
-      });
-    }
+		const match = await bcrypt.compare(password, admin.password);
+		if (!match) {
+			return res.status(400).json({
+				error: "wrong email or password",
+			});
+		}
 
-    const token = jwt.sign(
-      { userId: admin._id, email: admin.email },
-      process.env.ADMINJWTSECRET
-    );
+		const token = jwt.sign(
+			{ userId: admin._id, email: admin.email },
+			process.env.ADMINJWTSECRET
+		);
 
-    res.status(200).json({
-      email: admin.email,
-      token: token,
-    });
-  } catch (error) {
-    sendErrorEmail(email, "Someone tried to Login to Admin Panel");
-    res.status(500).json({
-      error: "Internal server error",
-    });
-  }
+		res.status(200).json({
+			email: admin.email,
+			token: token,
+		});
+	} catch (error) {
+		sendErrorEmail(email, "Someone tried to Login to Admin Panel");
+		res.status(500).json({
+			error: "Internal server error",
+		});
+	}
 };
 
 exports.GetAllVoices = async (req, res, next) => {
-  //send all available voices to frontend in the below format
-  res.status(200).json({
-    voices: [
-      {
-        voice_id: 2,
-        name: "jen-english",
-        is_custom: false,
-        reduce_latency: true,
-      },
-      {
-        voice_id: 0,
-        name: "matt",
-        is_custom: false,
-        reduce_latency: true,
-      },
-    ],
-  });
+	//send all available voices to frontend in the below format
+	res.status(200).json({
+		voices: [
+			{
+				voice_id: 2,
+				name: "jen-english",
+				is_custom: false,
+				reduce_latency: true,
+			},
+			{
+				voice_id: 0,
+				name: "matt",
+				is_custom: false,
+				reduce_latency: true,
+			},
+		],
+	});
 };
 exports.SingleCall = async (req, res, next) => {
-  console.log(req.body)
-  //single call here
-}
+	console.log("Received data:", req.body);
+	 // Parse the form values89
+	const { phone_number, prompt, transfer_number, voice, max_duration } = req.body;
+	const { country_code: countryCode, actual_phone_number: phoneNumber } = phone_number;
+	const { country_code: transferCountryCode, phone_number: transferPhoneNumber } = transfer_number;
+	const data = {
+		phone_number: countryCode+phoneNumber,
+		task: prompt,
+		voice_id: 1,
+		reduce_latency: false,
+		transfer_phone_number: transferCountryCode+transferPhoneNumber,
+	  };
+	
+	  // Dispatch the phone call
+	  axios
+		.post("https://api.bland.ai/call", data, {
+		  headers: {
+			authorization: apiKey,
+			"Content-Type": "application/json",
+		  },
+		})
+		.then((response) => {
+		  const { status } = response.data;
+	
+		  if (status) {
+			res
+			  .status(200)
+			  .send({ message: "Phone call dispatched", status: "success" });
+		  } else {
+			res
+			  .status(400)
+			  .send({ message: "Error dispatching phone call", status: "error" });
+		  }
+		})
+		.catch((error) => {
+		  console.error("Error:", error);
+	
+		  res
+			.status(400)
+			.send({ message: "Error dispatching phone call", status: "error" });
+		});
+
+};
